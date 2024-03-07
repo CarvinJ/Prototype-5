52a6318aaee4029716a48c3686ce846b7c9bf73b
CarvinJ <113186066+CarvinJ@users.noreply.github.com>
2024-03-07 12:21:14 -0500

    5.3 done

--- Assets/Scrpits/GameManger.cs
+++ Assets/Scrpits/GameManger.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
+    private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        yield return new WaitForSeconds(spawnRate);
+        int index = Random.Range(0, targets.Count);
+        Instantiate(taregts[index]);
     }
 }
